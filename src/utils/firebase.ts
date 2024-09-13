583a5cbfe74f821bb4ad1e72004f15bbbdf15346
Sai Tanush <saitanush789@gmail.com>
2024-09-13 21:41:59 +0530

    chore: added .env file to secure apikeys

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDkWfubVqvH4AaVs4Q3OE1tJ4hpyBCCZVU",
-  authDomain: "netflixgpt-157e3.firebaseapp.com",
-  projectId: "netflixgpt-157e3",
-  storageBucket: "netflixgpt-157e3.appspot.com",
-  messagingSenderId: "201265056099",
-  appId: "1:201265056099:web:258d58a52faac9e84eeb56",
-  measurementId: "G-EJKC1S04BB"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MSG_SEND_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
+
+console.log("apiKey = ", firebaseConfig.apiKey);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
