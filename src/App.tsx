1307b954741a4196ab07a0d9d1da6a8b0b2957cb
Sai Tanush <saitanush789@gmail.com>
2024-09-13 20:11:55 +0530

    chore: add navigate so that user can navigate to browse page when user is logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { Provider, useDispatch } from "react-redux";
 import { useEffect } from "react";
@@ -9,9 +9,11 @@ import ForgotPassword from "./components/ForgotPassword";
 import appStore from "./utils/appStore";
 import { auth } from "./utils/firebase";
 import { addUser, removeUser } from "./utils/userSlice";
+import Browse from "./components/Browse";
 
 function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -19,6 +21,7 @@ function App() {
         const { uid, email, displayName } = user;
         console.log(`user signed in : uid = ${uid}, email= ${email}, displayName= ${displayName}`);
         dispatch(addUser({uid: uid, email: email, displayName: displayName}));
+        navigate("/browse");
       }else{
         //User signed out
         console.log("user signed out ");
@@ -33,6 +36,7 @@ function App() {
         <Route path="/" element={<Body />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/browse" element={<Browse />} />
         <Route path="*" element={<Body />} />
       </Routes>
       </Provider>
