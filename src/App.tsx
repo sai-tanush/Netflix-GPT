00e2350d81ff0c5e68181580438437b82ce0105a
Sai Tanush <saitanush789@gmail.com>
2024-09-13 19:53:35 +0530

    chore: added store scope to entire application

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import Body from "./components/Body";
 import "./index.css";
 import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     < div>
+    <Provider store={appStore}>
       <Routes>
         <Route path="/" element={<Body />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="*" element={<Body />} />
       </Routes>
+      </Provider>
     </div>
   );
 }
