0d51523185517d01824b6236a073ea0aa2bfcdec
Sai Tanush <saitanush789@gmail.com>
2024-09-13 20:23:12 +0530

    added provider in main.jsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App.tsx";
+import appStore from "./utils/appStore.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={appStore}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
