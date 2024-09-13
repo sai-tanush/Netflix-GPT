c0971910c9449ec3f6fb5c83d933b9c2a2391ed8
Sai Tanush <saitanush789@gmail.com>
2024-09-13 20:24:11 +0530

    chore: added Browse page and signout button to navigate it to login page

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { removeUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 const Browse:React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleSignOut(){
+    dispatch(removeUser());
+    navigate("/login");
+  }
   return (
-    <div>Browse</div>
+    <div className='w-screen h-screen flex flex-col justify-center items-center'>
+    <p className='text-3xl font-bold'>Browse</p>
+    <button className='w-auto p-2 my-4 bg-red-700 rounded-lg text-white' onClick={handleSignOut}>SignOut</button>
+    </div>
   )
 }
 
