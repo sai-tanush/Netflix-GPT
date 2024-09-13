9bced44e24c827da4b8702cdee6bc9fcc348a8ab
Sai Tanush <saitanush789@gmail.com>
2024-09-13 19:19:07 +0530

    fix: removed unnecessary div tag

--- src/components/ui/HeaderLogo.tsx
+++ src/components/ui/HeaderLogo.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from '../../assets/logo.png';
+import React from "react";
+import logo from "../../assets/logo.png";
 
-const HeaderLogo:React.FC = () => {
+const HeaderLogo: React.FC = () => {
   return (
-    <div >
-      <img src={logo} alt='logo' height={100} width={190} className='absolute top-0 left-32 z-30 ' />
-    </div>
-  )
-}
+    <img
+      src={logo}
+      alt="logo"
+      height={100}
+      width={190}
+      className="absolute top-0 left-32 z-30 "
+    />
+  );
+};
 
 export default HeaderLogo;
