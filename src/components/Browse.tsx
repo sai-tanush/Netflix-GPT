c09ed6d41f64a28d59313fe6eea18c310e3f2c39
Sai Tanush <saitanush789@gmail.com>
2024-09-17 22:41:24 +0530

    chore: updated ui in browse page

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { removeUser } from '../utils/userSlice';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUser } from "../utils/userSlice";
+import { RootState } from "../utils/appStore";
 
-const Browse:React.FC = () => {
+const Browse: React.FC = () => {
+  const cartUser = useSelector((state: RootState) => state.user);
+
+  console.log("Browse Page Rendering ....");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  function handleSignOut(){
+  function handleSignOut() {
     dispatch(removeUser());
     navigate("/login");
+    console.log("Usercart inside Browse page = ", cartUser);
   }
-  return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center'>
-    <p className='text-3xl font-bold'>Browse</p>
-    <button className='w-auto p-2 my-4 bg-red-700 rounded-lg text-white' onClick={handleSignOut}>SignOut</button>
-    </div>
-  )
-}
 
-export default Browse
+  return (
+    <>
+      <div className="relative w-screen h-screen flex flex-col justify-center items-center">
+        <p className="text-3xl font-bold">Browse</p>
+        <button
+          className="w-auto p-2 my-4 bg-red-700 rounded-lg text-white"
+          onClick={handleSignOut}
+        >
+          SignOut
+        </button>
+      </div>
+      <div className="absolute top-2 right-2">
+        <p>User-{cartUser.email}</p>
+      </div>
+    </>
+  );
+};
+
+export default Browse;
