6a31254ed9cb5c5f4245fbfa26fa7fbdb8db9457
Sai Tanush <saitanush789@gmail.com>
2024-09-17 22:40:32 +0530

    refactor: removed navigate as it is not suitable here

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,45 @@ import { useEffect } from "react";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
-import { auth } from "./utils/firebase";
 import { addUser, removeUser } from "./utils/userSlice";
 import Browse from "./components/Browse";
 import "./index.css";
+import ProtectedRoutes from "./components/ProtecteRoutes";
+import { auth } from "./utils/firebase";
 
 function App() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         //user signed in
         const { uid, email, displayName } = user;
         console.log(`user signed in : uid = ${uid}, email= ${email}, displayName= ${displayName}`);
         dispatch(addUser({uid, email, displayName}));
-        navigate("/browse");
+        console.log("user signed in");
+        
       }else{
         //User signed out
         console.log("user signed out ");
         dispatch(removeUser());
-        navigate("/login");
+        navigate("/login")
       }
     })
+    return () => unsubscribe();
   }, [])
   return (   
       <Routes>
         <Route path="/" element={<Body />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/browse" element={<Browse />} />
         <Route path="*" element={<Body />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/browse" element={<Browse />} />
+        </Route>
       </Routes>
   );
 }
 
 export default App;
 
-
-//-build login page/ signup page and resume from 01:27:16
