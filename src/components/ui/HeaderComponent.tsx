63c76ffb763f13cca30ef2ba80b45de6c94a4e58
Sai Tanush <saitanush789@gmail.com>
2024-09-18 17:00:04 +0530

    refactor: created a constant.ts file to keep strings like logo url

--- src/components/ui/HeaderComponent.tsx
+++ src/components/ui/HeaderComponent.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../utils/firebase";
 import { addUser, removeUser } from "../../utils/userSlice";
+import LOGO from "../../utils/constants";
 
 const HeaderComponent: React.FC = () => {
   const dispatch = useDispatch();
@@ -28,7 +29,7 @@ const HeaderComponent: React.FC = () => {
   return (
     <div>
       <img
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/04bef84d-51f6-401e-9b8e-4a521cbce3c5/null/IN-en-20240903-TRIFECTA-perspective_0d3aac9c-578f-4e3c-8aa8-bbf4a392269b_small.jpg"
+        src={LOGO}
         alt="background_img"
         height="100vh"
         className="relative bg-gradient-to-b from-black"
