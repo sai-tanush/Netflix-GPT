79c0990daa8909187ddc80051af8523c65402b77
Sai Tanush <saitanush789@gmail.com>
2024-09-19 20:19:05 +0530

    refactor: added navbar & removed unnecesary items

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "../utils/appStore";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
+import BrowseNavbar from "./BrowseNavbar";
 const Browse: React.FC = () => {
-  const cartUser = useSelector((state: RootState) => state.user);
+  //const cartUser = useSelector((state: RootState) => state.user);
   const navigate = useNavigate();
   useNowPlayingMovies();
 
@@ -20,8 +19,8 @@ const Browse: React.FC = () => {
 
   return (
     <>
-      <div className="relative w-screen h-screen flex flex-col justify-center items-center">
-        <p className="text-3xl font-bold">Browse</p>
+      <div className="relative w-screen h-screen">
+        <BrowseNavbar />
         <button
           className="w-auto p-2 my-4 bg-red-700 rounded-lg text-white"
           onClick={handleSignOut}
@@ -29,12 +28,11 @@ const Browse: React.FC = () => {
           SignOut
         </button>
       </div>
-      <div className="absolute top-2 right-2">
-        <p>User-<span className="text-black font-bold">{cartUser.displayName}</span></p>
-      </div>
     </>
   );
 };
 
 export default Browse;
+
+//01:04:55
 
