80e576cef8fa782089241b1ff68730604acdd22d
Sai Tanush <saitanush789@gmail.com>
2024-09-19 19:35:51 +0530

    chore: replaced fetching movies data logic with new custom hook

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../utils/appStore";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { API_OPTIONS } from "../utils/constants";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
-
+import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
 const Browse: React.FC = () => {
   const cartUser = useSelector((state: RootState) => state.user);
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async() => {
-    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
-    const jsonData = await data.json();
-    console.log("Movies List = ", jsonData.results);
-    dispatch(addNowPlayingMovies(jsonData.results));
-  }
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  },[]);
+  useNowPlayingMovies();
 
   function handleSignOut() {
     signOut(auth).then(() => {
