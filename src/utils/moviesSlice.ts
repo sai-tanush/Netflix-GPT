1cf13a20522fe1f0fc54f81c4fd130818671623f
Sai Tanush <saitanush789@gmail.com>
2024-09-19 19:25:05 +0530

    chore: added types of movies

--- src/utils/moviesSlice.ts
+++ src/utils/moviesSlice.ts
@@ -1,16 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const moviesSlice = createSlice({
-  name: "movies",
-  initialState: {
+interface Movie {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  }
+
+  interface MoviesState {
+    nowPlayingMovies: Movie[] | null;
+  }
+  
+  const initialState: MoviesState = {
     nowPlayingMovies: null,
-  },
+  };
+
+const moviesSlice = createSlice({
+  name:"movies",
+  initialState,
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
   },
 });
-
 export const { addNowPlayingMovies } = moviesSlice.actions;
 export default moviesSlice.reducer;
