53c8da93a686fefc4f4562674b039d5d119db8b6
Sai Tanush <saitanush789@gmail.com>
2024-09-21 22:57:20 +0530

    refactor: removed sign out feature from browse page as the feature is added in browse Navbar Profile

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,32 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { signOut } from "firebase/auth";
-import { auth } from "../utils/firebase";
 import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
 import BrowseNavbar from "./BrowseNavbar";
 const Browse: React.FC = () => {
-  //const cartUser = useSelector((state: RootState) => state.user);
-  const navigate = useNavigate();
   useNowPlayingMovies();
-
-  function handleSignOut() {
-    signOut(auth).then(() => {
-      navigate("/login");
-    }).catch((error) => {
-      console.log("error=",error);
-    });
-  }
 
   return (
     <>
       <div className="relative w-screen h-screen">
         <BrowseNavbar />
-        <button
-          className="w-auto p-2 my-4 bg-red-700 rounded-lg text-white"
-          onClick={handleSignOut}
-        >
-          SignOut
-        </button>
       </div>
     </>
   );
