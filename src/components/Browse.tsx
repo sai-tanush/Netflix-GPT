fc565d8d997ac6a2f7a36401038ac03b5b955900
Sai Tanush <saitanush789@gmail.com>
2024-09-22 17:19:48 +0530

    chore: adding MainContainer component in Browse page

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
 import BrowseNavbar from "./BrowseNavbar";
+import MainContainer from "./MainContainer";
 const Browse: React.FC = () => {
   useNowPlayingMovies();
 
   return (
     <>
-      <div className="relative w-screen min-h-screen bg-gray-950">
+      <div className="relative w-screen min-h-screen">
         <BrowseNavbar />
+        <MainContainer />
       </div>
     </>
   );
 };
 
 export default Browse;
-
-//01:04:55
-
