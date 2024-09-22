28816bb191c5cbd5c36e66dc523456887ceebee1
Sai Tanush <saitanush789@gmail.com>
2024-09-22 17:19:07 +0530

    chore: displaying details of the preview movie

--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -1,10 +1,28 @@
+import { Info, Play } from "lucide-react";
 
-const VideoTitle = () => {
+const VideoTitle = ({
+  title,
+  overview,
+}: {
+  title: string;
+  overview: string;
+}) => {
   return (
-    <div>
-        <h1>Video Title</h1>
+    <div className=" flex flex-col pt-40 px-16 w-2/5 z-0">
+      <h1 className="text-6xl font-bold mb-1">{title}</h1>
+      <p className="text-md">{overview}</p>
+      <div className="flex items-center justify-start space-x-4 cursor-pointer mt-4">
+        <div className="flex justify-center gap-2 w-[8rem] border border-black rounded-md py-2.5 px-4">
+          <Play fill="black" />
+          <button className=""> Play</button>
+        </div>
+        <div className="flex justify-center gap-2 w-[10rem] bg-gray-600 rounded-md py-2.5 px-4">
+          <Info stroke="white" />
+          <button className="text-white"> More info</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoTitle
+export default VideoTitle;
