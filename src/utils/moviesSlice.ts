88398ffc4f054779ba90d09505ab1c25172d9c6f
Sai Tanush <saitanush789@gmail.com>
2024-09-22 19:44:10 +0530

    chore: added trailerVideoID in our redux movieSlice

--- src/utils/moviesSlice.ts
+++ src/utils/moviesSlice.ts
@@ -1,38 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 interface Movie {
-    adult: boolean;
-    backdrop_path: string;
-    genre_ids: number[];
-    id: number;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    release_date: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
-  }
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
-  interface MoviesState {
-    nowPlayingMovies: Movie[] | null;
-  }
-  
-  const initialState: MoviesState = {
-    nowPlayingMovies: null,
-  };
+interface MovieTrailerId {
+  id: string;
+}
+
+interface MoviesState {
+  nowPlayingMovies: Movie[] | null;
+  trailerVideo: MovieTrailerId | null;
+}
+
+const initialState: MoviesState = {
+  nowPlayingMovies: null,
+  trailerVideo: null,
+};
 
 const moviesSlice = createSlice({
-  name:"movies",
+  name: "movies",
   initialState,
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    }
   },
 });
-export const { addNowPlayingMovies } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions;
 export default moviesSlice.reducer;
