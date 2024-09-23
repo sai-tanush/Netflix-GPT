f77d28d458840f24d35751cdf1ea2e0b804b34e4
Sai Tanush <saitanush789@gmail.com>
2024-09-23 18:22:35 +0530

    refactor: removed unnecessaru logs

--- src/hooks/useNowPlayingMovies.ts
+++ src/hooks/useNowPlayingMovies.ts
@@ -10,7 +10,6 @@ export const useNowPlayingMovies = () => {
     const getNowPlayingMovies = async() => {
       const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
       const jsonData = await data.json();
-      console.log("Movies List = ", jsonData.results);
       dispatch(addNowPlayingMovies(jsonData.results));
     }
   
