79738433a0369dc1c6b77bbd457e77ea75056ffd
Sai Tanush <saitanush789@gmail.com>
2024-09-23 19:58:30 +0530

    chore: added autoplay & loop

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -12,7 +12,7 @@ const VideoBackground = ({ movieId }: { movieId: number }) => {
     <div>
       <iframe
         className="w-screen aspect-video"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1`}
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1&loop=1&playlist=${trailerVideo?.key}`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
