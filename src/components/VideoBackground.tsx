28a20e1ee74b9ade0b0981f8fd702f9d8a894bea
Sai Tanush <saitanush789@gmail.com>
2024-09-23 18:23:17 +0530

    refactor: imported new cutom hook

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,45 +1,12 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import { useSelector } from "react-redux";
 import { RootState } from "../utils/appStore";
+import useMovieTrailer from "../hooks/useMovietrailer";
 
-interface VideoDetails {
-  id: string;
-  iso_639_1: string;
-  iso_3166_1: string;
-  key: string;
-  name: string;
-  official: boolean;
-  published_at: string;
-  site: string;
-  size: number;
-  type: string;
-}
 
-const VideoBackground = ({ movieId }: { movieId: number }) => {
-  
-  const dispatch = useDispatch();
-  const trailerVideo = useSelector((store:RootState)=> store.movies?.trailerVideo)
-  //fetch trailer video
-  const getMovieVideo = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos`,
-      API_OPTIONS
-    );
-    const jsonData = await data.json();
-    console.log("Movie Details = ", jsonData);
-    const filterData = jsonData.results.filter(
-      (video: VideoDetails) => video.type === "Trailer"
-    );
-    const trailer = filterData.length ? filterData[0] : jsonData.results[0];
-    console.log("trailer = ", trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
 
-  useEffect(() => {
-    getMovieVideo();
-  }, []);
+const VideoBackground = ({ movieId }: { movieId: number }) => {  
+  useMovieTrailer({movieId});
+  const trailerVideo = useSelector((store:RootState)=> store.movies?.trailerVideo)  
 
   return (
     <div>
