b03c3a1a79ef0b9bab4e77134de7802796602b6e
Sai Tanush <saitanush789@gmail.com>
2024-09-23 20:14:32 +0530

    chore: added secondary container to render small movie cards

---
+++ src/components/SecondaryContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MovieList from './MovieList'
+import { RootState } from '../utils/appStore'
+import { useSelector } from 'react-redux'
+
+const SecondaryContainer:React.FC = () => {
+    const movies = useSelector((store:RootState) => store.movies)
+  return (
+    <div>
+        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+    </div>
+  )
+}
+
+export default SecondaryContainer
