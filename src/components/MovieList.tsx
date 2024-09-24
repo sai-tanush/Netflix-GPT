437a9ad15edc106e6aa0c605866a4e2c5d3c6bab
Sai Tanush <saitanush789@gmail.com>
2024-09-24 14:37:17 +0530

    refactor: imported Movie type

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,30 +1,18 @@
 import React from 'react'
-
-interface Movie {
-    adult: boolean;
-    backdrop_path: string;
-    genre_ids: number[];
-    id: number;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    release_date: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
-  }
+import MovieCard from './MovieCard';
+import { Movie } from '../utils/moviesSlice';
 
 interface MovieListProps {
     title: string
-    movies: Movie[] | null;
+    movies: Movie[] | null
 }
 
 const MovieList:React.FC<MovieListProps>= ({title, movies}) => {
+  console.log("movie Title = ", title);
   return (
-    <div>{title}</div>
+    <div>
+        <MovieCard movie={movies[0]} />
+    </div>
   )
 }
 
