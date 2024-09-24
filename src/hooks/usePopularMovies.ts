f5a28ded67a67517ee1b3a7d0e226e54288695b8
Sai Tanush <saitanush789@gmail.com>
2024-09-24 18:08:24 +0530

    chore: created new custom hook for popular movies

---
+++ src/hooks/usePopularMovies.ts
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { addPopularMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+
+export const usePopularMovies = () => {
+    
+    const dispatch = useDispatch();
+
+    const getPopularMovies = async() => {
+      const data = await fetch("https://api.themoviedb.org/3/movie/popular?page=1", API_OPTIONS);
+      const jsonData = await data.json();
+      dispatch(addPopularMovies(jsonData.results));
+    }
+  
+    useEffect(() => {
+      getPopularMovies();
+    },[]);      
+}
