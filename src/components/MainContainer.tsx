66758dcca2e8aa4fc6519ac7060138469fc797a8
Sai Tanush <saitanush789@gmail.com>
2024-09-24 15:43:29 +0530

    refactor: maincontainer position removed

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -10,7 +10,7 @@ const MainContainer:React.FC = () => {
     const mainMovie = movies[0];
     const { original_title, overview, id } = mainMovie;
   return (
-    <div className="relative flex flex-col">
+    <div className="flex flex-col">
         <VideoTitle title={original_title} overview={overview}/>
         <VideoBackground movieId={id} />
     </div>
