a9097735ae5851585df3e510fc29223f32e98b81
Sai Tanush <saitanush789@gmail.com>
2024-09-24 17:37:39 +0530

    style: adjusted position of first set of movie cards

--- src/components/SecondaryContainer.tsx
+++ src/components/SecondaryContainer.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import MovieList from './MovieList'
-import { RootState } from '../utils/appStore'
-import { useSelector } from 'react-redux'
+import React from "react";
+import MovieList from "./MovieList";
+import { RootState } from "../utils/appStore";
+import { useSelector } from "react-redux";
 
-const SecondaryContainer:React.FC = () => {
-    const movies = useSelector((store:RootState) => store.movies)
+const SecondaryContainer: React.FC = () => {
+  const movies = useSelector((store: RootState) => store.movies);
   return (
-      movies.nowPlayingMovies && ( 
-    <div className='bg-black -mt-72 relative z-50'>
+    movies.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="relative -mt-72 z-50">
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Upcomming"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Action"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Drama"} movies={movies.nowPlayingMovies} />
-    </div>
+          <MovieList title={"Upcomming"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Action"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Drama"} movies={movies.nowPlayingMovies} />
+        </div>
+      </div>
     )
-  )
-}
+  );
+};
 
-export default SecondaryContainer
+export default SecondaryContainer;
