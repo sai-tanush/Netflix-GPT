84e93793ef0ca84d7ecada0cc41f458e49be0158
Sai Tanush <saitanush789@gmail.com>
2024-09-24 18:09:11 +0530

    chore: called both custom hooks

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -3,8 +3,12 @@ import { useNowPlayingMovies } from "../hooks/useNowPlayingMovies";
 import BrowseNavbar from "./BrowseNavbar";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import { usePopularMovies } from "../hooks/usePopularMovies";
+import { useTopRatedMovies } from "../hooks/useTopRatedMovies";
 const Browse: React.FC = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
 
   return (
     <>
