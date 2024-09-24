a8f2dfacad641f97ad7516eb20ac5ddf0b92199d
Sai Tanush <saitanush789@gmail.com>
2024-09-24 17:22:46 +0530

    chore: adding logic to render movie cards only when movies data is present

--- src/components/SecondaryContainer.tsx
+++ src/components/SecondaryContainer.tsx
@@ -6,9 +6,15 @@ import { useSelector } from 'react-redux'
 const SecondaryContainer:React.FC = () => {
     const movies = useSelector((store:RootState) => store.movies)
   return (
+      movies.nowPlayingMovies && ( 
     <div className='bg-black'>
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Upcomming"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Action"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Drama"} movies={movies.nowPlayingMovies} />
     </div>
+    )
   )
 }
 
