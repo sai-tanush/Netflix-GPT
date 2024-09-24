80ef5933db1e57dac82bacd24fcf9660adcfd1c3
Sai Tanush <saitanush789@gmail.com>
2024-09-24 18:15:17 +0530

    chore: created new custom hook for upcoming movies

---
+++ src/hooks/useUpcomingMovies.ts
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { addUpcommingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+
+export const useUpcomingMovies = () => {
+    
+    const dispatch = useDispatch();
+
+    const getUpcomingMovies = async() => {
+      const data = await fetch("https://api.themoviedb.org/3/movie/upcoming?page=1", API_OPTIONS);
+      const jsonData = await data.json();
+      dispatch(addUpcommingMovies(jsonData.results));
+    }
+  
+    useEffect(() => {
+        getUpcomingMovies();
+    },[]);      
+}
