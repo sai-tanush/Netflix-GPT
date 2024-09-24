5f4549db977637d44e54e0f280506e517c8e92bd
Sai Tanush <saitanush789@gmail.com>
2024-09-24 18:30:10 +0530

    chore: created new custom hook for TV Shows

---
+++ src/hooks/useAiringTodayTVshows.ts
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { addAiringTodayTVshows } from "../utils/moviesSlice";
+
+export const useAiringTodayTVshows = () => {
+    
+    const dispatch = useDispatch();
+
+    const getAiringTodayTVshows = async() => {
+      const data = await fetch("https://api.themoviedb.org/3/tv/airing_today?page=1", API_OPTIONS);
+      const jsonData = await data.json();
+      dispatch(addAiringTodayTVshows(jsonData.results));
+    }
+  
+    useEffect(() => {
+      getAiringTodayTVshows();
+    },[]);      
+}
