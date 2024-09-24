2349d40b0bd508f55156634fc06f2fdee0c2954a
Sai Tanush <saitanush789@gmail.com>
2024-09-24 17:15:18 +0530

    refactor: update gap between cards

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,7 +7,7 @@ interface PosterPathProps {
 
 const MovieCard:React.FC<PosterPathProps>= ({ posterPath }) => {
   return (
-    <div className='w-48'>
+    <div className='w-48 pr-4'>
       <img alt="Move Poster" src={IMG_CDN_URL + posterPath} />
     </div>
   )
