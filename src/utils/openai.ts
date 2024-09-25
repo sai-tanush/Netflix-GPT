5e82a10b4b3e7fa4f69d6f87a00de3d8b3659850
Sai Tanush <saitanush789@gmail.com>
2024-09-25 18:23:59 +0530

    added api key for openai api

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    apiKey: 'sk-PSnhJjIgPeiDlMue4SWYp37f-40ptsyBEZLr-ymVYET3BlbkFJ-2OGfAR6GlQk4NAx3O9n_NJlhJ5ieo39UtgNrhp4IA',
     dangerouslyAllowBrowser: true,
 })
 
