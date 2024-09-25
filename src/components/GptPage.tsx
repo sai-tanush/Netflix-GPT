1e40b173bdf4f32af1ad33aea2ed7a2918c5f3c6
Sai Tanush <saitanush789@gmail.com>
2024-09-25 13:20:39 +0530

    chore: added bg image to gpt page

--- src/components/GptPage.tsx
+++ src/components/GptPage.tsx
@@ -1,13 +1,23 @@
-import GptMovieSuggesstions from "./GptMovieSuggesstions"
-import GptSearchBar from "./GptSearchBar"
+import NETFLIX_BG from "../utils/constants";
+import GptMovieSuggesstions from "./GptMovieSuggesstions";
+import GptSearchBar from "./GptSearchBar";
 
-const GptPage:React.FC = () => {
+const GptPage: React.FC = () => {
   return (
-    <div className="w-screen bg-black">
-        <GptSearchBar />
-        <GptMovieSuggesstions />
+    <div className="w-screen min-h-screen">
+      <div className="absolute -z-30">
+        <img
+          src={NETFLIX_BG}
+          alt="background_img"
+          height="100vh"
+          className="relative bg-gradient-to-b from-black"
+        />
+        <div className="absolute top-0 left-0 w-screen h-[100%] bg-black bg-opacity-70"></div>
+      </div>
+      <GptSearchBar />
+      <GptMovieSuggesstions />
     </div>
-  )
-}
+  );
+};
 
-export default GptPage
+export default GptPage;
