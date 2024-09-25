c5270b1d1fdd5e3172ac2cd0688f45d6a7da99f5
Sai Tanush <saitanush789@gmail.com>
2024-09-25 13:12:24 +0530

    chore: adjusted searchBar position

--- src/components/GptSearchBar.tsx
+++ src/components/GptSearchBar.tsx
@@ -1,8 +1,8 @@
 
 const GptSearchBar = () => {
   return (
-    <div className="relative pt-[20%] flex justify-center">
-        <form className="absolute top-44 bg-black py-2 px-4 rounded-md w-1/2 grid grid-cols-12">
+    <div className="pt-[8%] flex justify-center">
+        <form className="bg-black py-2 px-4 rounded-md w-1/2 grid grid-cols-12">
             <input type="text" className="px-3 col-span-9 h-[2.9rem] rounded-md" placeholder="What would you like to watch today>" />
             <button className="px-4 py-2 ml-3 col-span-3 h-[2.9rem] bg-red-700 rounded-md text-white">Search</button>
         </form>
