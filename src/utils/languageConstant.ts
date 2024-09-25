15f9963d7d6612826937ead5174a32f38dee4e46
Sai Tanush <saitanush789@gmail.com>
2024-09-25 16:09:32 +0530

    chore: added multilanguage constant

---
+++ src/utils/languageConstant.ts
@@ -0,0 +1,28 @@
+const lang = {
+    en: {
+        search: "Search",
+        gptSearchPlaceholder: "What would you like to watch today?"
+    },
+    hindi: {
+        search: "खोज",
+        gptSearchPlaceholder: "आज आप क्या देखना चाहेंगे?"
+    },
+    spanish: {
+        search: "Buscar",
+        gptSearchPlaceholder: "Qué te gustaría ver hoy?"
+    },
+    german: {
+        search: "Suchen",
+        gptSearchPlaceholder: "Was möchtest du heute sehen?"
+    },
+    japanese: {
+        search: "検索",
+        gptSearchPlaceholder: "今日は何を見たいですか？"
+    },
+    chinese: {
+        search: "搜尋",
+        gptSearchPlaceholder: "你今天想看什麼？"
+    }
+}
+
+export default lang;
