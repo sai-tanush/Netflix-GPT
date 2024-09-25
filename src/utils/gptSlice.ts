259331a69883fa8d241bd71bae0671632d786828
Sai Tanush <saitanush789@gmail.com>
2024-09-25 12:37:57 +0530

    chore: added gptSlice & reducer to store

---
+++ src/utils/gptSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type GptState = {
+  showGptSearch: boolean
+};
+
+const initialState: GptState = {
+    showGptSearch: false,
+}
+const gptSlice = createSlice({
+  name: "gpt",
+  initialState,
+  reducers: {
+    toggleGptSearchView: (state) => {
+       state.showGptSearch = !state.showGptSearch;
+    },
+  },
+});
+
+export const { toggleGptSearchView} = gptSlice.actions;
+export default gptSlice.reducer;
