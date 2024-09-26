9723ddb62b03038dacefc7313ba73621ce39866d
Sai Tanush <saitanush789@gmail.com>
2024-09-26 10:58:53 +0530

    chore: movies posters displayed on gptPage

--- src/components/GptMovieSuggesstions.tsx
+++ src/components/GptMovieSuggesstions.tsx
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux"
+import { RootState } from "../utils/appStore"
+import MovieList from "./MovieList";
 
 const GptMovieSuggesstions:React.FC = () => {
+  const { movies, gptMovies } = useSelector((store: RootState) => store.gpt);
+  if(!gptMovies) return null;
+
   return (
-    <div>GptMovieSuggesstions</div>
+    <div className="p-4 my-2 mx-3 bg-black text-white bg-opacity-55">
+      <div>
+        {gptMovies.map((movieName, index) => (
+          <MovieList key={movieName} title={movieName} movies={movies[index]} />
+        ))}
+      </div>
+    </div>
   )
 }
 
