e5b3fcba99df6c3a9632c5fa70c8928c17a2b3e1
Sai Tanush <saitanush789@gmail.com>
2024-09-26 11:09:44 +0530

    refactor: updated bg opacity

--- src/components/GptMovieSuggesstions.tsx
+++ src/components/GptMovieSuggesstions.tsx
@@ -7,7 +7,7 @@ const GptMovieSuggesstions:React.FC = () => {
   if(!gptMovies) return null;
 
   return (
-    <div className="p-4 mt-2 mx-3 bg-black text-white bg-opacity-50">
+    <div className="p-4 mt-2 mx-3 bg-black text-white bg-opacity-70">
       <div>
         {gptMovies.map((movieName, index) => (
           <MovieList key={movieName} title={movieName} movies={movies[index]} />
