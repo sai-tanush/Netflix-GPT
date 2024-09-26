560326c8863162f08983935ac57e3972f24da713
Sai Tanush <saitanush789@gmail.com>
2024-09-26 13:22:59 +0530

    fix: deleted openai file as we no longer need it

--- src/utils/openai.ts
+++
@@ -1,8 +0,0 @@
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-    apiKey: 'sk-PSnhJjIgPeiDlMue4SWYp37f-40ptsyBEZLr-ymVYET3BlbkFJ-2OGfAR6GlQk4NAx3O9n_NJlhJ5ieo39UtgNrhp4IA',
-    dangerouslyAllowBrowser: true,
-})
-
-export default openai;
