100d143b22eada7e71dd82952e7f5986ab4662a9
Sai Tanush <saitanush789@gmail.com>
2024-09-26 10:39:38 +0530

    chore: added new reducer to store tmdbmovie results

--- src/utils/gptSlice.ts
+++ src/utils/gptSlice.ts
@@ -1,11 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+
+type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+type MoviesCollection = Movie[][];
+
 type GptState = {
   showGptSearch: boolean
+  movies: MoviesCollection
 };
 
 const initialState: GptState = {
     showGptSearch: false,
+    movies: [],
 }
 const gptSlice = createSlice({
   name: "gpt",
@@ -14,8 +37,11 @@ const gptSlice = createSlice({
     toggleGptSearchView: (state) => {
        state.showGptSearch = !state.showGptSearch;
     },
+    addGptMovies: (state, action) => {
+      state.movies = action.payload;
+    }
   },
 });
 
-export const { toggleGptSearchView} = gptSlice.actions;
+export const { toggleGptSearchView, addGptMovies } = gptSlice.actions;
 export default gptSlice.reducer;
