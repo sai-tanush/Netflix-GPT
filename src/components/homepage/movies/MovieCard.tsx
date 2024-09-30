2bb0619dafb1fe900019ad08f012342d482461cd
Sai Tanush <saitanush789@gmail.com>
2024-09-30 15:32:50 +0530

    refactor: proper path in movies  & updated imports

---
+++ src/components/homepage/movies/MovieCard.tsx
@@ -0,0 +1,16 @@
+import { IMG_CDN_URL } from "../../../utils/constants";
+
+type PosterPathProps = {
+  posterPath: string;
+};
+
+const MovieCard: React.FC<PosterPathProps> = ({ posterPath }) => {
+  if (!posterPath) return null;
+  return (
+    <div className="w-36 md:w-48 pr-4">
+      <img alt="Move Poster" src={IMG_CDN_URL + posterPath} />
+    </div>
+  );
+};
+
+export default MovieCard;
