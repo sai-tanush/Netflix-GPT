ef3cf2e4381b9456fdeaafc7ab1761c8a8d7b9f1
Sai Tanush <saitanush789@gmail.com>
2024-09-30 15:05:28 +0530

    fix: ensuring proper use of .env

--- src/utils/geminiai.ts
+++ src/utils/geminiai.ts
@@ -1,7 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
-const geminiAPI = import.meta.env.VITE_GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(geminiAPI);
+const genAI = new GoogleGenerativeAI(`${import.meta.env.VITE_GEMINI_API_KEY}`);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 export default model;
