f44a529c7c11363c360a4433736878dd32ab189f
Sai Tanush <saitanush789@gmail.com>
2024-10-01 16:59:35 +0530

    chore: added currMovie type and dispalyed it when clicked

--- src/components/homepage/movies/MovieCard.tsx
+++ src/components/homepage/movies/MovieCard.tsx
@@ -1,13 +1,19 @@
 import { IMG_CDN_URL } from "../../../utils/constants";
+import { IMovieDetails, ITVShowDetails } from "../../../utils/moviesSlice";
 
 type PosterPathProps = {
   posterPath: string;
+  currMovie: IMovieDetails | ITVShowDetails;
 };
 
-const MovieCard: React.FC<PosterPathProps> = ({ posterPath }) => {
+const MovieCard: React.FC<PosterPathProps> = ({ posterPath, currMovie }) => {
   if (!posterPath) return null;
+
+  const handlePosterCardClicked = () => {
+    console.log("Current Movie Details = ", currMovie);
+  };
   return (
-    <div className="w-36 md:w-48 pr-4">
+    <div className="w-36 md:w-48 pr-4 cursor-pointer" onClick={handlePosterCardClicked}>
       <img alt="Move Poster" src={IMG_CDN_URL + posterPath} />
     </div>
   );
