e01f53b71fb8d82d122d46d9ec0e56f77be9e066
S SAI TANUSH <saitanush789@gmail.com>
2024-10-22 20:34:28 +0530

    refactor: sanitation checks

--- src/components/homepage/SecondaryContainer.tsx
+++ src/components/homepage/SecondaryContainer.tsx
@@ -7,7 +7,7 @@ const SecondaryContainer: React.FC = () => {
   const movies = useSelector((store: RootState) => store.movies);
   return (
     movies.nowPlayingMovies && (
-      <div className="bg-black">
+      <div className="bg-black mt-[5%]">
         <div className="relative md:-mt-72 z-50">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Upcoming"} movies={movies.upcommingMovies} />
