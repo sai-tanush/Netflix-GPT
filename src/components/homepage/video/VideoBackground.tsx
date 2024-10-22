61fa424c27cbb6df6533a89386be1d904e7d48cf
S SAI TANUSH <saitanush789@gmail.com>
2024-10-22 20:34:36 +0530

    refactor: sanitation checks

--- src/components/homepage/video/VideoBackground.tsx
+++ src/components/homepage/video/VideoBackground.tsx
@@ -14,7 +14,7 @@ const VideoBackground: React.FC<VideoBackgroundProps> = ({ movieId }) => {
 
   return (
     <iframe
-      className="w-screen aspect-video"
+      className="w-screen aspect-video mt-[-8rem]"
       src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1&loop=1&playlist=${trailerVideo?.key}`}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
